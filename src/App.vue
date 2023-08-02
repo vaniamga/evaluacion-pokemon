ea457fda8355f70d9471c7aab08f967038c72039
vaniamga <vaniagallardo578@gmail.com>
2023-08-02 08:14:46 -0600

    .

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,38 @@ import FormLogin from './components/FormLogin.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
 
     <div class="wrapper">
-      
+      <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+        </a>
 
-      <nav>
-        <RouterLink to="/">registro</RouterLink>
-        <RouterLink to="/loging">login</RouterLink>
-        <RouterLink to="/contenido">contenido</RouterLink>
-      </nav>
-    </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/">Registro</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/login">Login</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/contenido">Contenido</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </header>
+    </div>
 
   <RouterView />
 </template>
